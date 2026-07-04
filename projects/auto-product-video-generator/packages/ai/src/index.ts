167f69556826be64443d347ef83ebe59e4f52319
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2026-07-04 17:17:30 +0900

    productのvideo自動的に作成するツールもsubmoduleとして外に出した

--- projects/auto-product-video-generator/packages/ai/src/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './llm/provider.js';
-export * from './pipeline/analyzer.js';
-export * from './pipeline/scenario-generator.js';
-export * from './pipeline/subtitle-generator.js';
-export * from './pipeline/timeline-builder.js';
--- projects/auto-product-video-generator/packages/core/src/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './types/config.js';
-export * from './types/scenario.js';
-export * from './types/timeline.js';
-export * from './types/project.js';
-export * from './utils/config-loader.js';
-export * from './utils/file-helpers.js';
-export * from './utils/logger.js';
